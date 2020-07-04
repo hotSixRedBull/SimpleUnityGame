d34c20bf461d1991809c2046f5cf41a252a6d279
hotSixRedBull <akkalee1207@gmail.com>
2020-07-04 23:26:04 +0900

    change ipad flow

--- Assets/Data/touch_open.cs
+++ Assets/Data/touch_open.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI; //Text, Image 등의UI관련 변수 등을 사용할수 있게됩니다
 
-public class touch_app : MonoBehaviour
+public class touch_open : MonoBehaviour
 {
     public Image targetImage;
-    public Sprite toOpenAppThree;
-    public Sprite toOpenAppFour;
+    public Sprite appImageFour; //바뀌어질 이미지
     // Start is called before the first frame update
     void Start()
     {
@@ -20,18 +19,16 @@ public class touch_app : MonoBehaviour
         
     }
 
-    public void touchApp() {
-        Debug.Log("touchApp");
+    public void touchOpen() {
+        Debug.Log("touchOpen");
         Debug.Log("Inventory.ipadStep");
         Debug.Log(Inventory.ipadStep);
         Debug.Log("Inventory.resolution");
         Debug.Log(Inventory.resolution);
         if(Inventory.ipadStep == 4 && Inventory.resolution == 3) {
-            targetImage.sprite = toOpenAppThree;
-        } else if(Inventory.ipadStep == 4 && Inventory.resolution == 4) {
-            targetImage.sprite = toOpenAppFour;
-            if(Inventory.resolution <= 5) {
-                Inventory.resolution = 5;
+            targetImage.sprite = appImageFour;
+            if(Inventory.resolution <= 4) {
+                Inventory.resolution = 4;
             }
         }
     }
