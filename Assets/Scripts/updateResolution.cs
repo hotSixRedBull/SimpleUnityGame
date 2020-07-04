ffa992e8f37eb387bf5c5f4e230d1b347506c492
hotSixRedBull <akkalee1207@gmail.com>
2020-07-04 22:41:18 +0900

    update resolution2

---
+++ Assets/Scripts/updateResolution.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //Text, Image 등의UI관련 변수 등을 사용할수 있게됩니다
+
+public class updateResolution : MonoBehaviour
+{
+    public Image targetImage;
+    public Sprite resTwo; //바뀌어질 이미지
+    public Sprite resThree; //바뀌어질 이미지
+    public Sprite resFour; //바뀌어질 이미지
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Inventory.resolution == 2) {
+            targetImage.sprite = resTwo;
+        } else if(Inventory.resolution == 3) {
+            targetImage.sprite = resThree;
+        } else if(Inventory.resolution >= 4) {
+            targetImage.sprite = resFour;
+        }
+    }
+}
